f831019d0cecdfb7df890e3469b6fa8a993fca25
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-12-27 19:31:45 +0530

    Update matrix.cpp

--- addons/matrix.cpp
+++ addons/matrix.cpp
@@ -13,15 +13,24 @@ struct Mat {
     a = vector<vector<T>>(n, vector<T>(m, val));
   }
 
+  Mat(const vector<T>& oth) {
+    a = vector<vector<T>>(sz(oth), vector<T>(1));
+    n = sz(oth);
+    m = 1;
+    for (int i = 0; i < sz(oth); i++) {
+      a[i][0] = oth[i];
+    }
+  }
+
   Mat(const vector<vector<T>>& oth) {
     a = oth;
     n = sz(a);
     m = sz(a[0]);
   }
 
-  Mat<T> operator*(const Mat<T>& oth) const {
+  Mat operator*(const Mat& oth) const {
     assert(m == oth.n);
-    Mat<T> r(n, oth.m);
+    Mat r(n, oth.m);
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < oth.m; j++) {
         for (int k = 0; k < m; k++) {
@@ -32,16 +41,21 @@ struct Mat {
     return r;
   }
 
-  template<class U> friend Mat<T> power(const Mat<T>& a, const U& b) {
+  Mat& operator*=(const Mat& oth) {
+    *this = (*this) * oth;
+    return *this;
+  }
+
+  template<class U> friend Mat power(const Mat& a, const U& b) {
     assert(b >= 0);
     assert(a.n == a.m);
-    Mat<T> x = a;
-    Mat<T> res(a.n, a.m);
+    Mat x = a;
+    Mat res(a.n, a.m);
     for (int i = 0; i < a.n; i++) res.a[i][i] = 1;
     U p = b;
     while (p > 0) {
-      if (p & 1) res = res * x;
-      x = x * x;
+      if (p & 1) res *= x;
+      x *= x;
       p >>= 1;
     }
     return res;
