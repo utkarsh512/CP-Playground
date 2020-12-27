4b091b8595915cf5aa1e11d34847eaa71f99ce0d
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-12-27 18:52:31 +0530

    Update spf.cpp

--- algo/number theory/primes/spf.cpp
+++ algo/number theory/primes/spf.cpp
@@ -6,7 +6,7 @@ vector<int> spf(int n) {
   for (int i = 2; i < n; i++) {
     if (a[i] == 0) {
       a[i]  = i;
-      for (int j = 2 * i; j < n; j++) {
+      for (int j = 2 * i; j < n; j += i) {
         if (a[j] == 0) a[j] = i;
       }
     }
