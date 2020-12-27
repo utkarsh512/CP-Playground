0c835b30ca7c3acd8bf2275ee6b99b09c7cb0066
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-12-27 18:47:40 +0530

    Update and rename smallestPrimeFactor.cpp to spf.cpp

---
+++ algo/number theory/primes/spf.cpp
@@ -0,0 +1,15 @@
+// Computing smallest prime factor as sieve in O(nloglogn)
+vector<int> spf(int n) {
+  n++;
+  vector<int> a(n);
+  a[1] = 1;
+  for (int i = 2; i < n; i++) {
+    if (a[i] == 0) {
+      a[i]  = i;
+      for (int j = 2 * i; j < n; j++) {
+        if (a[j] == 0) a[j] = i;
+      }
+    }
+  }
+  return a;
+}
