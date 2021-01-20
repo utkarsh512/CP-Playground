5487dd51e25ce403616975e9445878c32af4b873
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-01-20 11:23:17 +0530

    Update debug.cpp

--- addons/debug.cpp
+++ addons/debug.cpp
@@ -1,4 +1,3 @@
-#ifdef LOCAL
 string to_string(const string& s) {return '"' + s + '"';}
 string to_string(const char* s) {return to_string((string) s);}
 string to_string(bool b) {return (b ? "true" : "false");}
@@ -8,17 +7,21 @@ template <class A, class B> string to_string(pair<A, B> p) {return "(" + to_stri
 template <class A, class B, class C> string to_string(tuple<A, B, C> p) {return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";}
 template <class A, class B, class C, class D> string to_string(tuple<A, B, C, D> p) {return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";}
 template <class A> string to_string(A v) {bool first = true;string res = "{";for (const auto &x : v) {if (!first) {res += ", ";}first = false;res += to_string(x);}res += "}";return res;}
-#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
 template <class Arg1>
 void __f(const char* name, Arg1&& arg1){
   cerr << name << " : " << to_string(arg1) << "\n";
 }
+
 template <class Arg1, class... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args){
   const char* comma = strchr(names + 1, ',');
   cerr.write(names, comma - names) << " : " << to_string(arg1) <<" | ";
   __f(comma+1, args...);
 }
+
+#ifdef LOCAL
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
 #else
 #define debug(...) 69
 #endif
