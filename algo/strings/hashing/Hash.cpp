61ae61fa0109f5b3f57b8dc7430c51bee5653048
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-01-23 10:58:07 +0530

    integrated with Mint

--- algo/strings/hashing/Hash.cpp
+++ algo/strings/hashing/Hash.cpp
@@ -1,56 +1,141 @@
-const int p = 67;
-const int maxn = 1e5 + 10;
-long long rdx[maxn], rdxi[maxn];
+#ifdef int
+  #undef int
+#endif
 
-struct custom_hash {
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
+template <int MOD_> struct modnum {
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+private:
+  using ll = long long;
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
   }
- 
-  size_t operator()(uint64_t x) const {
-    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-    return splitmix64(x + FIXED_RANDOM);
+public:
+  modnum() : v(0) {}
+  modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+  explicit operator int() const { return v; }
+  friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+  friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+  friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+  friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
   }
+  friend modnum inv(const modnum& m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD-v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum& m) { return m.neg(); }
+  modnum operator- () const {
+    return neg();
+  }
+  modnum operator+ () const {
+    return modnum(*this);
+  }
+  modnum& operator ++ () {
+    v ++;
+    if (v == MOD) v = 0;
+    return *this;
+  }
+  modnum& operator -- () {
+    if (v == 0) v = MOD;
+    v --;
+    return *this;
+  }
+  modnum& operator += (const modnum& o) {
+    v += o.v;
+    if (v >= MOD) v -= MOD;
+    return *this;
+  }
+  modnum& operator -= (const modnum& o) {
+    v -= o.v;
+    if (v < 0) v += MOD;
+    return *this;
+  }
+  modnum& operator *= (const modnum& o) {
+    v = int(ll(v) * ll(o.v) % MOD);
+    return *this;
+  }
+  modnum& operator /= (const modnum& o) {
+    return *this *= o.inv();
+  }
+  friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+  friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+  friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+  friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+  friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+  friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+  friend string to_string (const modnum& a) { return to_string(int(a)); }
 };
-template <class key, class val>
-using hash_table = gp_hash_table<key, val, custom_hash>;
 
-hash_table<char, int> M;
+using Mint = modnum<md>;
+
+vector<Mint> fact(1, 1);
+vector<Mint> inv_fact(1, 1);
+
+template<class T, class U>
+T power(const T& a, const U& b) {
+  assert(b >= 0);
+  T x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+  }
+  return res;
+}
+ 
+Mint C(int n, int k) {
+  if (k < 0 || k > n) {
+    return 0;
+  }
+  while ((int) fact.size() < n + 1) {
+    fact.push_back(fact.back() * (int) fact.size());
+    inv_fact.push_back(1 / fact.back());
+  }
+  return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
+
+Mint p = 67;
+Mint pinv = p.inv();
+vector<Mint> rdx(1, 1), rdxi(1, 1);
+
+map<char, int> M;
+
+bool pre_calc = false;
 
 void pre() {
   int i = 1;
   for (char j = '0'; j <= '9'; j++) M[j] = i++;
   for (char j = 'A'; j <= 'Z'; j++) M[j] = i++;
   for (char j = 'a'; j <= 'z'; j++) M[j] = i++;
-
-  rdx[0] = 1;
-  rdxi[0] = inv(rdx[0]);
-  for (int i = 1; i < maxn; i++) {
-    rdx[i] = rdx[i - 1] * p % md;
-    rdxi[i] = inv(rdx[i]);
-  }
 }
 
 struct Hash {
   string s;
   int length;
-  vector<long long> pref, suf;
+  vector<Mint> pref, suf;
 
   void fillprefHash() {
-    long long h = 0;
+    Mint h = 0;
     for (int i = 0; i < length; i++) {
-      h = (h + (M[s[i]]) * rdx[i]) % md;
+      h = (h + rdx[i] * M[s[i]]);
       pref[i] = h;
     }
   }
 
   void fillsufHash() {
-    long long h = 0;
+    Mint h = 0;
     for (int i = 0; i < length; i++) {
-      h = (h + (M[s[length - 1 - i]]) * rdx[i]) % md;
+      h = (h + rdx[i] * M[s[length - 1 - i]]);
       suf[i] = h;
     }
   }
@@ -58,29 +143,35 @@ struct Hash {
   Hash() {}
 
   Hash(string _s) : s(_s){
+    if (!pre_calc) {
+      pre_calc = 1;
+      pre();
+    }
     length = sz(s);
+    while (sz(rdx) < length) {
+      rdx.pb(rdx.back() * p);
+      rdxi.pb(rdxi.back() * pinv);
+    }
     pref.assign(length, 0);
     suf.assign(length, 0);
     fillprefHash();
     fillsufHash();
   }
 
-  long long fwd (int i, int j) {
-    long long left = (i != 0)? pref[i - 1]: 0;
-    long long right = pref[j];
-    long long h = right - left;
-    if (h < 0) h += md;
-    return (h * rdxi[i] % md);
+  Mint fwd (int i, int j) {
+    Mint left = (i != 0)? pref[i - 1]: 0;
+    Mint right = pref[j];
+    Mint h = right - left;
+    return h * rdxi[i];
   }
 
-  long long bwd (int i, int j) {
+  Mint bwd (int i, int j) {
     swap(i, j);
     i = length - 1 - i;
     j = length - 1 - j;
-    long long left = (i != 0)? suf[i - 1]: 0;
-    long long right = suf[j];
-    long long h = right - left;
-    if (h < 0) h += md;
-    return (h * rdxi[i] % md);
+    Mint left = (i != 0)? suf[i - 1]: 0;
+    Mint right = suf[j];
+    Mint h = right - left;
+    return h * rdxi[i];
   }
 };
