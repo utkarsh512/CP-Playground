fc345c438c611ed480e9be5f16060de857859904
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-04-25 06:20:58 +0530

    Update trie_bit.cpp for memory issues

--- ds/trie/trie_bit.cpp
+++ ds/trie/trie_bit.cpp
@@ -1,46 +1,56 @@
-template <int SZ>
-struct IntegerTrie {
-  /**
-   * Description: Trie data structure for bit manipulation
-   * Source: Self
-   */
-
-  struct Node {
-    int v;
-    int nxt[2];
-  } lt[40 * SZ];
-
-  int tot = 0;
-
-  int newNode() {
-    lt[tot].v = 0;
-    lt[tot].nxt[0] = -1;
-    lt[tot].nxt[1] = -1;
-    return tot++;
-  } 
-
-  IntegerTrie() {}
-
-  void update (int rt, int x, int v) {
-    for (int i = 31; i >= 0; i--) {
-      int id = (x >> i) & 1;
-      if (lt[rt].nxt[id] == -1) {
-        lt[rt].nxt[id] = newNode();
-      }
-      rt = lt[rt].nxt[id];
-      lt[rt].v += v;
+struct Node {
+  int cnt[2];
+  Node *pt[2];
+  Node() {
+    for (int i = 0; i < 2; i++) {
+      cnt[i] = 0;
+      pt[i] = nullptr;
     }
   }
+};
 
-  int query (int rt, int x) {
-    int ans = 0;
-    for (int i = 31; i >= 0; i--) {
-      int id = (x >> i) & 1;
-      int crt = lt[rt].nxt[id ^ 1];
-      if (crt != -1 && lt[crt].v > 0) ans |= (1ll << i);
-      else crt = lt[rt].nxt[id];
-      rt = crt;
+template <int BITS>
+struct Trie {
+  Node *root, *cur;
+  Trie() {
+    root = new Node();
+  }
+  void insert(int x) {
+    insert(root, x, BITS - 1);
+  }
+  void remove(int x) {
+    remove(root, x, BITS - 1);
+  }
+  int max_xor(int x) {
+    return max_xor(root, x, BITS - 1);
+  }
+  bool insert(Node *cur, int x, int b) {
+    if (b == -1) return true;
+    bool c = (x & (1 << b))? 1: 0;
+    if (cur->pt[c] == nullptr) {
+      cur->pt[c] = new Node();
     }
-    return ans;
+    cur->cnt[c]++;
+    return insert(cur->pt[c], x, b - 1);
+  }
+  void remove(Node *cur, int x, int b) {
+    if (b == -1) {
+      return;
+    }
+    bool c = (x & (1 << b))? 1: 0;
+    cur->cnt[c]--;
+    remove(cur->pt[c], x, b - 1);
+  }
+  int max_xor(Node *cur, int x, int b) {
+    if (b == -1) {
+      return 0;
+    }
+    bool c = (x & (1 << b))? 1: 0;
+    c ^= 1;
+    if (cur->cnt[c]) {
+      return (1 << b) + max_xor(cur->pt[c], x, b - 1);
+    } else {
+      return max_xor(cur->pt[c ^ 1], x, b - 1);
+    }
   }
 };
