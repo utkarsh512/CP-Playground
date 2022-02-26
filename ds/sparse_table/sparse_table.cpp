fdf65181654f193db58939b846f7d15751f48c22
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-02-26 09:56:48 +0530

    Update sparse_table.cpp

--- ds/sparse_table/sparse_table.cpp
+++ ds/sparse_table/sparse_table.cpp
@@ -1,33 +1,27 @@
-/**
- * Description: Computing min/max function in constant time
- * Source: tourist
- */
-#ifdef int
-  #undef int
-#endif
 template <class T, class F = function<T (const T&, const T&)>>
-struct SparseTable {
+class sparse_table {
   int n;
   vector<vector<T>> st;
   F func;
 
-  SparseTable () {}
+public:
+  sparse_table() {}
 
-  SparseTable (const vector<T>& a, const F& f) : func(f) {
+  sparse_table(const vector<T>& a, const F& f) : func(f) {
     n = static_cast<int>(a.size());
-    int maxlg = 32 - __builtin_clz(n);
+    int maxlg = 64 - __builtin_clzll(n);
     st.resize(maxlg);
     st[0] = a;
     for (int j = 1; j < maxlg; j++) {
       st[j].resize(n - (1 << j) + 1);
       for (int i = 0; i <= n - (1 << j); i++) {
-        st[j][i] = func(st[j-1][i], st[j-1][i + (1 << (j-1))]);
+        st[j][i] = func(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
       }
     }
   }
 
-  T get (int l, int r) const {
-    int lg = 32 - __builtin_clz(r - l + 1) - 1;
+  T query(int l, int r) const {
+    int lg = 64 - __builtin_clzll(r - l + 1) - 1;
     return func(st[lg][l], st[lg][r - (1 << lg) + 1]);
   }
 };
